ecc2ca5349a37ad19ed82e54f40e3b5363fea8e5
MrH-rezroll <mhodge8414@gmail.com>
2022-12-15 15:24:36 -0800

    Wireframe CSS for Advanced view created, no advanced options yet

--- src/App.tsx
+++ src/App.tsx
@@ -82,8 +82,35 @@ function Beginner() {
 function Advanced() {
   return (
     <div id="Advanced">
-      <h2>Advanced</h2>
-      <Link to="/">Back</Link>
+      <div id="EncryptionSelection">
+        <h2>Beginner</h2>
+        <Link to="/">Back</Link>
+        <form>
+          <legend>Encryption Type:</legend>
+          <select defaultValue={"caesar"}>
+            <option value="caesar">Caesar Cipher</option>
+          </select>
+        </form>
+      </div>
+      <div id="EncryptionOperations">
+        <form>
+          <legend>Encryption Options:</legend>
+          <label htmlFor="shiftby">Shift by:</label>
+          <input type="number" id="shiftby" name="shiftby" min="1" max="24" defaultValue={7} onChange={changeShiftValue}></input>
+          <label>Result:</label>
+          <input type="text" readOnly={true} value="a -> h"></input>
+        </form>
+        <form>
+          <div>
+            <legend>Encryption Text:</legend>
+            <textarea name="textToEncode"></textarea>
+          </div>
+          <div>
+            <button>Encode Text</button>
+            <button>Decode Text</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
